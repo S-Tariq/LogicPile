449864b7d8e7ff084e748c1d09eccd97ecaac7e6
S Tariq <sameentariq@hotmail.co.uk>
2018-03-28 23:12:26 +0100

    Completed EggDrop

---
+++ source/EggDrop/src/com/stariq/EggDrop.java
@@ -0,0 +1,27 @@
+package com.stariq;
+
+public class EggDrop {
+
+    public boolean eggCracks(int floor) {
+        return floor >= 49;
+    }
+
+    public int findFloor(int totalFloors) {
+        int low = 0;
+        int high = totalFloors;
+        while (low < high) {
+            int mid = (low+high)/2;
+            if (eggCracks(mid)) {
+                high = mid;
+            } else {
+                low = mid+1;
+            }
+            System.out.println("[low]: " + low + ", [high]: " + high);
+        }
+        return low;
+    }
+
+    public int maxIterations(int totalFloors){
+        return (int)Math.ceil(Math.log(totalFloors)/Math.log(2));
+    }
+}
---
+++ source/EggDrop/src/com/stariq/Main.java
@@ -0,0 +1,18 @@
+package com.stariq;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    addEggs();
+    }
+
+    public static void addEggs(){
+
+        EggDrop binaryEggDrop = new EggDrop();
+        int totalFloors = 100;
+        int maxFloor = binaryEggDrop.findFloor(totalFloors);
+        System.out.println("Floor the egg breaks on: \n" + maxFloor);
+        int maxIterations = binaryEggDrop.maxIterations(totalFloors);
+        System.out.println("Iterations required: \n" + maxIterations);
+    }
+}
