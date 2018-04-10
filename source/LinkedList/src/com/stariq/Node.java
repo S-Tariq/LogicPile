6f74f9d398654f227406798d3b1a22b48acbf178
S Tariq <sameentariq@hotmail.co.uk>
2018-04-10 21:01:19 +0100

    Added prepend method

--- source/LinkedList/src/com/stariq/Node.java
+++ source/LinkedList/src/com/stariq/Node.java
@@ -8,12 +8,29 @@ public class Node {
     public Node(int data){
         this.data = data;
     }
+}
+
+class LinkedList{
+
+    Node head;
 
     public void append(int data){
-        Node current = this;
+
+        if(head == null){
+            head = new Node(data);
+            return;
+        }
+        Node current = head;
         while(current.next != null){
             current = current.next;
         }
         current.next = new Node(data);
+    }
+
+    public void prepend(int data){
+        
+        Node newHead = new Node(data);
+        newHead.next = head;
+        head = newHead;
     }
 }
