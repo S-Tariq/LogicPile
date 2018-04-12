2ea7f84df6ded2079cfc654cf8cf06c44b3339c7
S Tariq <sameentariq@hotmail.co.uk>
2018-04-12 12:05:41 +0100

    Added insert and show methods

--- source/LinkedList/src/com/stariq/CustomLinkedList.java
+++ source/LinkedList/src/com/stariq/CustomLinkedList.java
@@ -5,6 +5,7 @@ public class CustomLinkedList {
     Node head;
 
     public void insert(int data){
+
         Node node = new Node();
         node.data = data;
         node.next = null;
@@ -13,6 +14,21 @@ public class CustomLinkedList {
             head = node;
         } else {
             Node newData = head;
+            while(newData.next != null){
+                newData = newData.next;
+            }
+            newData.next = node;
         }
+    }
+
+    public void show(){
+
+        Node node = head;
+
+        while(node.next != null){
+            System.out.println(node.data);
+            node = node.next;
+        }
+        System.out.println(node.data);
     }
 }
--- source/LinkedList/src/com/stariq/Main.java
+++ source/LinkedList/src/com/stariq/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+    CustomLinkedList list = new CustomLinkedList();
+    list.insert(2);
+    list.insert(5);
+    list.insert(7);
 
+    list.show();
 
     }
 }
