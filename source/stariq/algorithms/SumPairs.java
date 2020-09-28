3f0fa6f8d6ef22e4e6b35b096730fc7a9489871d
S Tariq <sameentariq@hotmail.co.uk>
2020-09-29 00:55:28 +0100

    Made changes to sum pairs algo

--- source/stariq/algorithms/SumPairs.java
+++ source/stariq/algorithms/SumPairs.java
@@ -1,16 +1,23 @@
 package stariq.algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SumPairs {
 
     public static void main(String[] args) {
         int[] array = new int[]{2,4,3,7,2,1,4,5};
-        SumPairs.findPairsForSum(array,8);
-        SumPairs.findPairsForSum(array,4);
-        SumPairs.findPairsForSum(array,28);
+        findPairsUsingLoops(array,8);
+        findPairsUsingLoops(array,4);
+        findPairsUsingLoops(array,28);
+        System.out.println("-------------");
+        findPairsUsingList(array,8);
+        findPairsUsingList(array,4);
+        findPairsUsingList(array,28);
     }
 
     // Using loops
-    public static void findPairsForSum(int[] arr, int sum) {
+    public static void findPairsUsingLoops(int[] arr, int sum) {
         int count = 0;
         String pairs = "";
         for(int i = 0; i < arr.length; i++) {
@@ -23,9 +30,27 @@ public class SumPairs {
             }
         }
         if(count == 0) {
-            System.out.println("No pairs found for sum " + sum);
+            System.out.println("No pairs found for sum of " + sum);
         } else {
             System.out.println("Sum of " + sum + " has " + count + " pairs: " + pairs);
         }
+    }
+
+    public static void findPairsUsingList(int[] arr, int sum) {
+        List<Integer> elements = new ArrayList<>();
+        String pairs = "";
+        for(int i = 0; i < arr.length; i++) {
+            if(elements.contains(sum - arr[i])) {
+                pairs += "[" + arr[i] + "," + (sum-arr[i]) + "] ";
+            } else {
+                elements.add(arr[i]);
+            }
+        }
+        if(pairs.equals("")) {
+            System.out.println("No pairs found for sum of " + sum);
+        } else {
+            System.out.println("Sum of " + sum + " has pairs: " + pairs);
+        }
+
     }
 }
