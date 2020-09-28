5ec77eb1a86468bf8be6b1d3f11e92cca9c1e76e
S Tariq <sameentariq@hotmail.co.uk>
2020-09-29 00:55:19 +0100

    Made changes to first repetition algo

--- source/stariq/algorithms/FirstRepetition.java
+++ source/stariq/algorithms/FirstRepetition.java
@@ -6,17 +6,26 @@ import java.util.List;
 public class FirstRepetition {
 
     public static void main(String[] args) {
-        char result = FirstRepetition.firstRepeatedCharacter("albksdfkarlb");
+        char result = firstRepeatedUsingList("albksdfkarlb");
         System.out.println(result);
-        result = FirstRepetition.firstRepeatedCharacter("abcal");
+        result = firstRepeatedUsingList("abcal");
         System.out.println(result);
-        result = FirstRepetition.firstRepeatedCharacter("tyopky");
+        result = firstRepeatedUsingList("tyopky");
         System.out.println(result);
-        result = FirstRepetition.firstRepeatedCharacter("qwpaslkpe");
+        result = firstRepeatedUsingList("qwpaslkpe");
         System.out.println(result);
+        System.out.println("------------");
+        char result2 = firstRepeatedUsingArray("albksdfkarlb");
+        System.out.println(result2);
+        result2 = firstRepeatedUsingArray("abcal");
+        System.out.println(result2);
+        result2 = firstRepeatedUsingArray("tyopky");
+        System.out.println(result2);
+        result2 = firstRepeatedUsingArray("qwpaslkpe");
+        System.out.println(result2);
     }
 
-    public static char firstRepeatedCharacter(String str) {
+    public static char firstRepeatedUsingList(String str) {
 
         List<Character> allChars = new ArrayList<>();
         char c = str.charAt(0);
@@ -28,7 +37,22 @@ public class FirstRepetition {
             }
             c = str.charAt(i);
         }
-
         return c;
+
+    }
+
+    public static char firstRepeatedUsingArray(String str) {
+
+        int[] array = new int[26];
+        char c = str.charAt(0);
+        for(int i = 1; i < str.length(); i++) {
+            array[c - 97] += 1;
+            if(array[c - 97] > 1) {
+                break;
+            }
+            c = str.charAt(i);
+        }
+        return c;
+
     }
 }
