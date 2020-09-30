7b2eb133a9bfd46be57f07d48166cb38f9a0004f
S Tariq <sameentariq@hotmail.co.uk>
2020-09-30 02:17:03 +0100

    Implemented merge sorted linkedlist algorithm

---
+++ source/stariq/algorithms/MergeSortedLinkedLists.java
@@ -0,0 +1,87 @@
+package stariq.algorithms;
+
+import stariq.datastructures.SinglyLinkedList;
+
+public class MergeSortedLinkedLists {
+
+    public static void main(String[] args) {
+
+        SinglyLinkedList first = new SinglyLinkedList(1);
+        SinglyLinkedList head1 = first;
+        first.next = new SinglyLinkedList(3);
+        first = first.next;
+        first.next = new SinglyLinkedList(6);
+        first = first.next;
+        first.next = new SinglyLinkedList(7);
+        first = first.next;
+        first.next = new SinglyLinkedList(9);
+
+        SinglyLinkedList second = new SinglyLinkedList(0);
+        SinglyLinkedList head2 = second;
+        second.next = new SinglyLinkedList(2);
+        second = second.next;
+        second.next = new SinglyLinkedList(4);
+        second = second.next;
+        second.next = new SinglyLinkedList(5);
+        second = second.next;
+        second.next = new SinglyLinkedList(8);
+        second = second.next;
+        second.next = new SinglyLinkedList(10);
+
+        printList(head1);
+        printList(head2);
+        printList(merge(head1,head2));
+
+    }
+
+    public static SinglyLinkedList merge(SinglyLinkedList list1, SinglyLinkedList list2) {
+
+        SinglyLinkedList current = new SinglyLinkedList();
+
+        if(list1.data < list2.data) {
+            current.data = list1.data;
+            list1 = list1.next;
+        } else {
+            current.data = list2.data;
+            list2 = list2.next;
+        }
+
+        SinglyLinkedList head = current;
+
+        while(list1 != null && list2 != null) {
+            if(list1.data < list2.data) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        while(list1 != null) {
+            current.next = list1;
+            list1 = list1.next;
+            current = current.next;
+        }
+
+        while(list2 != null) {
+            current.next = list2;
+            list2 = list2.next;
+            current = current.next;
+        }
+
+        return head;
+    }
+
+    public static void printList(SinglyLinkedList list) {
+        StringBuilder elements = new StringBuilder();
+        while(list != null) {
+            elements.append(list.data);
+            elements.append(" ");
+            list = list.next;
+        }
+        System.out.println(elements.toString());
+    }
+}
+
