6d0a38ecb91d1bca15993dd8a880790ff1bc6537
S Tariq <sameentariq@hotmail.co.uk>
2020-10-01 01:13:45 +0100

    Quick and Merge sort implemented

---
+++ source/stariq/algorithms/MergeSort.java
@@ -0,0 +1,65 @@
+package stariq.algorithms;
+
+// Merge sort is a divide and conquer algorithm.
+// It divides the array in two halves, calls itself for the two halves
+// and then merges the two sorted halves.
+// Time complexity: O(nlog(n))
+// Space complexity: O(n)
+public class MergeSort {
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {3,7,6,1,9,7,5,4,2,1,0};
+        // sorted = {0,1,1,2,3,4,5,6,7,7,9}
+        sort(arr, arr.length);
+        for(int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void sort(int[] arr, int length) {
+        if(length < 2) {
+            return;
+        }
+
+        int mid = length/2;
+        int[] left = new int[mid];
+        int[] right = new int[length - mid];
+
+        for(int i = 0; i < length; i++) {
+            if(i < mid) {
+                left[i] = arr[i];
+            } else {
+                right[i - mid] = arr[i];
+            }
+        }
+        sort(left,mid);
+        sort(right,length-mid);
+        merge(arr, left, right, mid, length-mid);
+    }
+    // Focuses on splitting the left side before the right side.
+    // When merge happens in left, it moves to right side to bring it to the same merge level.
+
+    private static void merge(int[] arr, int[] left, int[] right, int l, int r) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while(i < l && j < r) {
+            if(left[i] < right[j]) {
+                arr[k++] = left[i++];
+            } else {
+                arr[k++] = right[j++];
+            }
+        }
+
+        while(i < l) {
+            arr[k++] = left[i++];
+        }
+
+        while(j < r) {
+            arr[k++] = right[j++];
+        }
+    }
+    // Merges left and right side in order.
+    // First loop runs when both arrays have elements left to merge.
+    // Moves onto the last two loops depending on which array still has elements left.
+}
---
+++ source/stariq/algorithms/QuickSort.java
@@ -0,0 +1,56 @@
+package stariq.algorithms;
+
+// Quick sort is a divide and conquer algorithm.
+// It picks an element as a pivot and partitions the given array around the pivot.
+// Pivot can be the first or last element.
+// Time complexity: O(nlog(n))
+// Space complexity: O(n)
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {3,7,6,1,9,7,5,4,2,1,0};
+        // sorted = {0,1,1,2,3,4,5,6,7,7,9}
+        sort(arr, 0, arr.length - 1);
+        for(int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void sort(int[] arr, int start, int end) {
+        int pivotIndex = partition(arr, start, end);
+
+        if(pivotIndex - 1 > start) {
+            sort(arr, start, pivotIndex - 1);
+        }
+        if(pivotIndex + 1 < end) {
+            sort(arr, pivotIndex + 1, end);
+        }
+    }
+    // First if statement deals with the left side of the pivot.
+    // Second if statement deals with the right side of the pivot.
+    // The -1 & +1 is there to ignore the pivot element which already gets sorted by the partition.
+
+    private static int partition(int[] arr, int start, int end) {
+        int pivot = arr[end];
+        for(int i = start; i < end; i++) {
+            if(arr[i] < pivot) {
+                int temp = arr[i];
+                arr[i] = arr[start];
+                arr[start] = temp;
+                start++;
+            }
+        }
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        return start;
+    }
+    // Pivot: Last element
+    // The loop starts swapping all the elements less than the pivot by moving it to the left side.
+    // The start index gets incremented until it reaches the point where the pivot should be.
+    // After loop, the pivot element is moved to its correct position.
+
+
+}
+
+
