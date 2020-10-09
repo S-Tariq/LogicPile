a4638e53a4aa1fc87d3750e3fc876685dab19379
S Tariq <sameentariq@hotmail.co.uk>
2020-10-09 18:08:01 +0100

    Update TopFiveAverage.java

--- source/stariq/algorithms/TopFiveAverage.java
+++ source/stariq/algorithms/TopFiveAverage.java
@@ -2,42 +2,54 @@ package stariq.algorithms;
 
 import java.util.*;
 
+// Find the average score of each student's top five scores in the order of their student id.
 public class TopFiveAverage {
 
     public static void main(String[] args) {
         int[][] arr = new int[][] {{1,91}, {1,92}, {2,93}, {2,97}, {1,60}, {2,77},
                 {1,65}, {1,87}, {1,100}, {2,100}, {2,76}};
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
+
+        int[][] result = topFive(arr);
+        for(int i = 0; i < result.length; i++) {
+            for(int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
             }
             System.out.println();
         }
     }
 
     public static int[][] topFive(int[][] arr) {
-        Map<Integer, List<Integer>> record = new HashMap<>();
+        TreeMap<Integer, PriorityQueue<Integer>> recordsMap = new TreeMap<>();
 
-        for(int i = 0; i < arr.length; i++) {
-            List<Integer> scores = record.get(arr[i][1]);
-            if(scores == null) {
-                scores = new ArrayList<>();
-                scores.add(arr[i][1]);
-                record.put(arr[i][0], scores);
+        for(int[] item : arr) {
+            int id = item[0];
+            int score = item[1];
+            PriorityQueue<Integer> scoresQueue;
+            if(recordsMap.containsKey(id)) {
+                scoresQueue = recordsMap.get(id);
+                scoresQueue.offer(score);
+                if(scoresQueue.size() > 5) {
+                    scoresQueue.poll();
+                }
+                recordsMap.put(id, scoresQueue);
             } else {
-                scores.add(arr[i][1]);
+                scoresQueue = new PriorityQueue<>();
+                scoresQueue.offer(score);
+                recordsMap.put(id, scoresQueue);
             }
         }
 
-        for(Map.Entry<Integer, List<Integer>> e : record.entrySet()) {
-            if(e.getValue().size() <= 5) {
-                continue;
-            } else {
-                Collections.sort(e.getValue());
+        int[][] result = new int[recordsMap.size()][2];
+        for(int id : recordsMap.keySet()) {
+            PriorityQueue<Integer> scoresQueue = recordsMap.get(id);
+            int sum = 0;
+            while(!scoresQueue.isEmpty()) {
+                sum += scoresQueue.poll();
             }
+            result[id - 1][0] = id;
+            result[id - 1][1] = sum / 5;
+
         }
-
-
-        return new int[1][1];
+        return result;
     }
 }
