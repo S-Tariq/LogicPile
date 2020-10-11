47494f05933ea511fd3c33436282521fddfe350d
S Tariq <sameentariq@hotmail.co.uk>
2020-10-11 01:13:36 +0100

    Create RemoveDuplicatesInSortedArray.java

---
+++ source/stariq/algorithms/array/RemoveDuplicatesInSortedArray.java
@@ -0,0 +1,41 @@
+package stariq.algorithms.array;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class RemoveDuplicatesInSortedArray {
+
+    public static void main(String[] args) {
+        int[] array = new int[] {0,0,1,1,1,2,2,3,3,4};
+        int len = removeDuplicates2(array);
+        for(int i = 0; i < len; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        TreeMap<Integer, Integer> numCount = new TreeMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            numCount.put(num, numCount.getOrDefault(num, 0) + 1);
+        }
+
+        int i = 0;
+        for(Map.Entry<Integer, Integer> e: numCount.entrySet()) {
+            nums[i++] = e.getKey();
+        }
+
+        return numCount.size();
+    }
+
+    public static int removeDuplicates2(int[] nums) {
+        int len = 1;
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] != nums[i + 1]) {
+                len++;
+                nums[len - 1] = nums[i + 1];
+            }
+        }
+        return len;
+    }
+}
