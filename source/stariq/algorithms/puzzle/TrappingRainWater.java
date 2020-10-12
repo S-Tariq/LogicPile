7379a10423ae4dd50212f48b378000316b69c04c
S Tariq <sameentariq@hotmail.co.uk>
2020-10-12 03:49:50 +0100

    Update TrappingRainWater.java

--- source/stariq/algorithms/puzzle/TrappingRainWater.java
+++ source/stariq/algorithms/puzzle/TrappingRainWater.java
@@ -1,4 +1,30 @@
 package stariq.algorithms.puzzle;
 
 public class TrappingRainWater {
+
+    public static void main(String[] args) {
+        int[] array = new int[] {1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trappingWater(array));
+    }
+
+    public static int trappingWater(int[] arr) {
+        int volume = 0;
+        int left = 0;
+        int right = arr.length - 1;
+        int higher = 0;
+        while(left < right) {
+            int lower;
+            if(arr[left] < arr[right]) {
+                lower = arr[left];
+                left++;
+            } else {
+                lower = arr[right];
+                right--;
+            }
+            higher = Math.max(higher, lower);
+            volume += higher - lower;
+        }
+        return volume;
+
+    }
 }
