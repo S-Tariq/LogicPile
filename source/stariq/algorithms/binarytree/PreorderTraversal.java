ec7d27983d867d9faf6ad71edc9a48356489e7c3
S Tariq <sameentariq@hotmail.co.uk>
2020-10-12 03:49:39 +0100

    Update PreorderTraversal.java

--- source/stariq/algorithms/binarytree/PreorderTraversal.java
+++ source/stariq/algorithms/binarytree/PreorderTraversal.java
@@ -4,6 +4,7 @@ import stariq.datastructures.binarytree.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 // root -> left -> right
 // Preorder traversal of binary tree
@@ -20,13 +21,44 @@ public class PreorderTraversal {
         bst.right.left = new TreeNode(7);
         bst.right.right = new TreeNode(9);
 
-        for(int i : preorderTraversal(root)) {
+        for(int i : preorderIterative(root)) {
             System.out.print(i + " ");
         }
+        System.out.println();
+        preorderRecursive(root);
     }
 
-    public static List<Integer> preorderTraversal(TreeNode root) {
-        return new ArrayList<>();
+    public static List<Integer> preorderIterative(TreeNode root) {
+        List<Integer> bstList = new ArrayList<>();
+        Stack<TreeNode> bstStack = new Stack<>();
+
+        if(root == null) {
+            return bstList;
+        }
+
+        bstStack.push(root);
+
+        while(!bstStack.isEmpty()) {
+            TreeNode current = bstStack.pop();
+            bstList.add(current.val);
+            if(current.right != null) {
+                bstStack.push(current.right);
+            }
+            if(current.left != null) {
+                bstStack.push(current.left);
+            }
+        }
+        return bstList;
+    }
+
+    // Cannot use list unless made static as recursion will keep recreating the same list.
+    public static void preorderRecursive(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        System.out.print(root.val + " ");
+        preorderRecursive(root.left);
+        preorderRecursive(root.right);
     }
 
 }
