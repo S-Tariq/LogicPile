0df01c826c59b1952c8937f887e4ef3657894870
S Tariq <sameentariq@hotmail.co.uk>
2020-11-06 21:47:26 +0000

    Create MinStackUsingStack.java

---
+++ source/stariq/datastructures/stack/MinStackUsingStack.java
@@ -0,0 +1,41 @@
+package stariq.datastructures.stack;
+
+import java.util.Stack;
+
+public class MinStackUsingStack {
+
+    Stack<Integer> valueStack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+
+    public MinStackUsingStack() {
+
+    }
+
+    public void push(int val) {
+        valueStack.push(val);
+        if(minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            int min = minStack.peek();
+            if(val <= min) {
+                minStack.push(val);
+            }
+        }
+    }
+
+    public void pop() {
+        int pop = valueStack.pop();
+        int min = minStack.peek();
+        if(pop == min) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return valueStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
