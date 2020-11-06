98ab48e6a231dd8b80d02d04cb4b98a900382e61
S Tariq <sameentariq@hotmail.co.uk>
2020-11-06 20:45:28 +0000

    Update ReverseWords.java

--- source/stariq/algorithms/string/ReverseWords.java
+++ source/stariq/algorithms/string/ReverseWords.java
@@ -1,24 +1,39 @@
 package stariq.algorithms.string;
 
+// https://leetcode.com/problems/reverse-words-in-a-string/
 // Reverse words in a string.
 public class ReverseWords {
 
     public static void main(String[] args) {
-        ReverseWords.reverseWordsUsingLoop("you cant see me");
-        ReverseWords.reverseWordsUsingLoop("how are you");
+        reverseWordsSplitBySpace("you cant see me");
+        reverseWordsSplitBySpace("how are you");
+
+        System.out.println(reverseWordsIgnoringSpace("  hello  world"));
+
     }
 
-    public static void reverseWordsUsingLoop(String str) {
+    public static void reverseWordsSplitBySpace(String str) {
         String[] words = str.split(" ");
-        String reversed = "";
+        StringBuilder reversed = new StringBuilder();
         for(int i = words.length - 1; i >= 0; i--) {
             // This condition avoids space at the end of sentence.
             if(i == 0) {
-                reversed += words[i];
+                reversed.append(words[i]);
             } else {
-                reversed += words[i] + " ";
+                reversed.append(words[i]).append(" ");
             }
         }
-        System.out.println(reversed);
+        System.out.println(reversed.toString());
+    }
+
+    public static String reverseWordsIgnoringSpace(String str) {
+        String[] words = str.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = words.length - 1; i >= 0; i--) {
+            if(!words[i].isEmpty()) {
+                sb.append(words[i]).append(" ");
+            }
+        }
+        return sb.toString().trim();
     }
 }
