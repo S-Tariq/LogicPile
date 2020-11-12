9e7d92fb8df0d154e75d5566d3c25006d9fea98d
S Tariq <sameentariq@hotmail.co.uk>
2020-11-12 14:34:52 +0000

    Create ReverseInteger.java

---
+++ source/stariq/algorithms/others/ReverseInteger.java
@@ -0,0 +1,20 @@
+package stariq.algorithms.others;
+
+// https://leetcode.com/problems/reverse-integer/
+public class ReverseInteger {
+
+    public static void main(String[] args) {
+        System.out.println(reverse(123));
+    }
+
+    public static int reverse(int x) {
+        long reverse = 0;
+        while(x != 0) {
+            int remainder = x % 10;
+            x = x / 10;
+            reverse =  reverse * 10 + remainder;
+        }
+        // This portion takes care of integer overflows.
+        return (int)reverse == reverse ? (int)reverse : 0;
+    }
+}
