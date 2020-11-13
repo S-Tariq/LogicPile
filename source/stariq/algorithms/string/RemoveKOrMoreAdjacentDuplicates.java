ce630e6fa2ef8d6d09de5b52698f2c008b1b5f02
S Tariq <sameentariq@hotmail.co.uk>
2020-11-13 00:18:53 +0000

    Create RemoveKOrMoreAdjacentDuplicates.java

---
+++ source/stariq/algorithms/string/RemoveKOrMoreAdjacentDuplicates.java
@@ -0,0 +1,38 @@
+package stariq.algorithms.string;
+
+import java.util.Stack;
+
+// Similar to RemoveAdjacentDuplicates2 but you remove k or more duplicates.
+public class RemoveKOrMoreAdjacentDuplicates {
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates("iiiixxxxxiiccccczzffffflllllllllfffffllyyyyyuuuuuz", 3));
+    }
+
+    public static String removeDuplicates(String str, int k) {
+        Stack<Character> charStack = new Stack<>();
+        Stack<Integer> countStack = new Stack<>();
+        for(char c : str.toCharArray()) {
+            if(!charStack.isEmpty() && countStack.peek() >= k && charStack.peek() != c) {
+                charStack.pop();
+                countStack.pop();
+            }
+            if(charStack.isEmpty() || charStack.peek() != c) {
+                charStack.push(c);
+                countStack.push(1);
+            } else {
+                countStack.push(countStack.pop() + 1);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!charStack.isEmpty()) {
+            char c = charStack.pop();
+            int n = countStack.pop();
+            for(int i = 0; i < n; i++) {
+                sb.append(c);
+            }
+        }
+        return sb.reverse().toString();
+    }
+
+}
