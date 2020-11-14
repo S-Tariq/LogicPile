688667661c9b4148a340c06d96b3108fb727d4ac
S Tariq <sameentariq@hotmail.co.uk>
2020-11-14 22:46:41 +0000

    Update ZeroesAtEnd.java

--- source/stariq/algorithms/array/ZeroesAtEnd.java
+++ source/stariq/algorithms/array/ZeroesAtEnd.java
@@ -1,16 +1,32 @@
 package stariq.algorithms.array;
 
+// https://leetcode.com/problems/move-zeroes/
 public class ZeroesAtEnd {
 
     public static void main(String[] args) {
         int[] arr = new int[] {1,4,0,7,0,6,0,2,0,1};
-        zerosAtEnd(arr);
+       // arr = new int[] {0, 1, 0, 3, 12, 0};
+        zeroesAtEnd(arr);
         for(int i : arr) {
             System.out.print(i + " ");
         }
     }
 
-    public static void zerosAtEnd(int[] arr) {
+    public static void zeroesAtEnd(int[] arr) {
+        int j = 0;
+        for(int i = 0; i < arr.length ; i++) {
+            if(arr[i] != 0) {
+                if(i != j) {
+                    arr[j] = arr[i];
+                    arr[i] = 0;
+                }
+                j++;
+            }
+        }
+    }
+
+    // Order does not matter as long as all zeroes are at the end.
+    public static void zeroesAtEndNoOrder(int[] arr) {
         int j = arr.length - 1;
         for(int i = arr.length - 1; i >= 0; i--) {
             if(arr[i] == 0) {
