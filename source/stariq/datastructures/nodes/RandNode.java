97487151bb5ae6d6b603e3bce7da15e9207d2ca5
S Tariq <sameentariq@hotmail.co.uk>
2020-11-15 19:54:30 +0000

    Deep copy of linked list

---
+++ source/stariq/datastructures/nodes/RandNode.java
@@ -0,0 +1,15 @@
+package stariq.datastructures.nodes;
+
+public class RandNode {
+
+    public int val;
+    public RandNode next;
+    public RandNode random;
+
+    public RandNode(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+
+}
---
+++ source/stariq/datastructures/singlylinkedlist/DeepCopyWithRandomPointer.java
@@ -0,0 +1,61 @@
+package stariq.datastructures.singlylinkedlist;
+
+import stariq.datastructures.nodes.RandNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/problems/copy-list-with-random-pointer/
+public class DeepCopyWithRandomPointer {
+
+    public RandNode copyRandomList(RandNode head) {
+        if(head == null) {
+            return null;
+        }
+        Map<RandNode, RandNode> map = new HashMap<>();
+        RandNode current = head;
+        while(current != null) {
+            map.put(current, new RandNode(current.val));
+            current = current.next;
+        }
+
+        current = head;
+        while(current != null) {
+            RandNode node = map.get(current);
+            node.next = map.get(current.next);
+            node.random = map.get(current.random);
+            current = current.next;
+        }
+        return map.get(head);
+    }
+
+    public RandNode copyRandomList2(RandNode head) {
+
+        Map<RandNode,Integer> inputMap = new HashMap<>();
+        Map<Integer, RandNode> resultMap = new HashMap<>();
+        RandNode current = head;
+        RandNode result = new RandNode(0);
+        RandNode resultHead = result;
+        int count = 0;
+        while(current != null) {
+            inputMap.put(current, count);
+            result.next = new RandNode(current.val);
+            resultMap.put(count, result.next);
+            count++;
+            current = current.next;
+            result = result.next;
+        }
+
+        current = head;
+        result = resultHead.next;
+        while(current != null) {
+            if(current.random != null) {
+                Integer index = inputMap.get(current.random);
+                result.random = resultMap.get(index);
+            }
+            current = current.next;
+            result = result.next;
+        }
+        return resultHead.next;
+    }
+}
