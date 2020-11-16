0581b7c32d9f4ec076e6e4039de866ee31d0d2d5
S Tariq <sameentariq@hotmail.co.uk>
2020-11-16 01:01:26 +0000

    Create BinarySearchInSortedArray.java

---
+++ source/stariq/algorithms/array/BinarySearchInSortedArray.java
@@ -0,0 +1,26 @@
+package stariq.algorithms.array;
+
+// https://leetcode.com/problems/binary-search/
+public class BinarySearchInSortedArray {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{-1,0,3,5,9,12};
+        System.out.println(search(array, 9));
+    }
+
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high) {
+            int mid = (low + high)/2;
+            if(target > nums[mid]) {
+                low = mid + 1;
+            } else if (target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
