4dce5dc677359e509b49ae789b86aca2dd28dd93
S Tariq <sameentariq@hotmail.co.uk>
2020-11-16 15:45:28 +0000

    Create KthSmallestInBST.java

---
+++ source/stariq/datastructures/binarytree/KthSmallestInBST.java
@@ -0,0 +1,44 @@
+package stariq.datastructures.binarytree;
+
+import stariq.datastructures.nodes.TreeNode;
+
+import java.util.Stack;
+
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+public class KthSmallestInBST {
+
+    public static void main(String[] args) {
+        TreeNode bst = new TreeNode(5);
+        TreeNode root = bst;
+        bst.left = new TreeNode(3);
+        bst.left.left = new TreeNode(1);
+        bst.left.right = new TreeNode(4);
+        bst.right = new TreeNode(8);
+        bst.right.left = new TreeNode(7);
+        bst.right.right = new TreeNode(9);
+        System.out.println(secondSmallest(root, 2));
+    }
+
+    public static int secondSmallest(TreeNode root, int k) {
+        if(root == null) {
+            return -1;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        int count = 0;
+        while(current != null || !stack.isEmpty()) {
+            while(current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            count++;
+            if(count == k) {
+                break;
+            }
+            current = current.right;
+
+        }
+        return current.val;
+    }
+}
