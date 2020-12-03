2d12045b8c9a23b463f6a076a62847bfd7faf386
S Tariq <35509620+S-Tariq@users.noreply.github.com>
2020-12-03 22:29:33 +0000

    Create BottomView.java

---
+++ source/stariq/datastructures/binarytree/BottomView.java
@@ -0,0 +1,63 @@
+package stariq.datastructures.binarytree;
+
+import stariq.datastructures.nodes.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.TreeMap;
+
+public class BottomView {
+
+    public static void main(String[] args) {
+        TreeNode bst = new TreeNode(5);
+        TreeNode root = bst;
+        bst.left = new TreeNode(3);
+        bst.left.left = new TreeNode(1);
+        bst.left.right = new TreeNode(4);
+        bst.right = new TreeNode(8);
+        bst.right.left = new TreeNode(7);
+        bst.right.right = new TreeNode(9);
+
+        bottomView(root);
+    }
+
+    public static void bottomView(TreeNode root) {
+        class PosNode {
+            TreeNode node;
+            int position;
+
+            PosNode(TreeNode node, int position) {
+                this.node = node;
+                this.position = position;
+            }
+        }
+
+        if (root == null) {
+            return;
+        }
+
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        Queue<PosNode> queue = new LinkedList<PosNode>();
+
+        queue.add(new PosNode(root, 0));
+
+        while (!queue.isEmpty()) {
+            PosNode current = queue.poll();
+            treeMap.put(current.position, current.node.val);
+
+            if (current.node.left != null) {
+                int pos = current.position - 1;
+                queue.add(new PosNode(current.node.left, pos));
+            }
+            if (current.node.right != null) {
+                int pos = current.position + 1;
+                queue.add(new PosNode(current.node.right, pos));
+            }
+        }
+
+        for(Integer i : treeMap.values()) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
