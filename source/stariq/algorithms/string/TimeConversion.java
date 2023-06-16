32d5e221f28c00aa64b6801dd0da4b962dbd4c29
S Tariq <35509620+S-Tariq@users.noreply.github.com>
2023-06-16 21:21:48 +0100

    Create TimeConversion.java

---
+++ source/stariq/algorithms/string/TimeConversion.java
@@ -0,0 +1,35 @@
+package stariq.algorithms.string;
+
+// https://www.hackerrank.com/challenges/time-conversion/problem
+// Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+public class TimeConversion {
+
+    public static void main(String[] args) {
+        System.out.println(timeConversion("07:05:45PM"));
+        System.out.println(timeConversion("06:07:23AM"));
+        System.out.println(timeConversion("12:01:32PM"));
+        System.out.println(timeConversion("12:01:45AM"));
+    }
+
+    public static String timeConversion(String s) {
+        String converted = "";
+        char amOrPm = s.charAt(s.length() - 2);
+        if(amOrPm == 'P') {
+            String num = s.substring(0, 2);
+            if(num.equals("12")) {
+                converted = s.substring(0,s.length() - 2);
+            } else {
+                String number = String.valueOf(Integer.parseInt(num) + 12);
+                converted = s.replaceFirst(num, number).substring(0, s.length() - 2);
+            }
+        } else {
+            String num = s.substring(0, 2);
+            if(num.equals("12")) {
+                converted = s.replaceFirst("12", "00").substring(0,s.length() - 2);
+            } else {
+                converted = s.substring(0, s.length() - 2);
+            }
+        }
+        return converted;
+    }
+}
